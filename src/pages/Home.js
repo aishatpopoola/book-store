32028cd632411b1717b311ac770a9caeba584c9a
aishatpopoola <aishatpopoola906@gmail.com>
2022-07-19 21:55:01 +0100

    fix: fix linters error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import AddBookForm from '../components/AddBookForm';
 import Book from '../components/Book';
 
 const Home = () => {
+  // eslint-disable-next-line no-unused-vars
   const [bookList, setBookList] = useState([]);
   // const getBooks = () => {
   //   setBookList(books);
